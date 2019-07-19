e988f877f61da8950a8533e53169c28eafa707ce
Gallant003 <53094212+Gallant003@users.noreply.github.com>
2019-07-19 13:17:23 -0400

    Add files via upload
    I don't know what to write here

---
+++ Customer.java
@@ -0,0 +1,71 @@
+package com.service.RestfulWebService;
+
+
+public class Customer  {
+//Class variable, Member variable , properties of the class
+	public 	int id;
+	private 	double cost;
+	private 	String name;
+
+
+	
+ 
+ 
+	
+	//constructor
+	
+	public int getId() {
+	return id;
+}
+
+public void setId(int id) {
+	this.id = id;
+}
+
+public double getCost1() {
+	return cost;
+}
+
+public void setCost(double cost) {
+	this.cost = cost;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+
+
+
+
+	Customer(){}
+	
+	public Customer(int id,double cost,String name){
+		
+		this.id=id;
+		this.cost=cost;
+		this.name=name;
+	}
+	
+	
+	
+	double getCost()
+	{
+		return cost*.1;
+	}
+	
+	public void  printCustomerDetails()
+	{
+		String space="  ";
+		System.out.println(id+space+ name+space+cost);
+		
+	}
+	
+	
+
+}
+
